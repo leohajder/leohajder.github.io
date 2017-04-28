a7e56c0710cb0940d06fb454aef1e5ea95f64904
Leo Hajder <leohajder@gmail.com>
2017-04-28 14:36:23 +0200

    Update

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 $(function() {
-    var source = $("#repos-template").html();
-    var template = Handlebars.compile(source);
+    var sourceRepos = $("#repos-template").html();
+    var templateRepos = Handlebars.compile(sourceRepos);
     $.ajax({
         type: "GET",
         url: "https://api.github.com/users/leohajder/repos",
         success: function(response) {
-            var viewData = {
+            var dataRepos = {
                 repos: response
             }; //the JSON response from the GitHub API is unnamed, so I did this to make it easy to loop with Handlebars.js
-            $("#projects").append(template(viewData));
+            $("#github").append(templateRepos(dataRepos));
         },
         error: function() {
-            $("#projects").append("Sorry, something went wrong.");
+            $("#github").append("Sorry, something went wrong.");
         }
     });
 });
