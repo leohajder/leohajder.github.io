81f9aafa2b60c2a586cb315d88c8ba66c7b8fb58
Leo Hajder <leohajder@gmail.com>
2017-06-13 06:53:26 +0200

    Add age template, add fork icon to repos template.

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 $(function() {
+    var sourceAge = $("#age-template").html();
+    var templateAge = Handlebars.compile(sourceAge);
+    var myAge = new Date(new Date() - new Date('1987-11-14')).getFullYear() - 1970
+    var dataAge = {
+        age: myAge
+    };
+    $("#age").append(templateAge(dataAge));
+
     var sourceRepos = $("#repos-template").html();
     var templateRepos = Handlebars.compile(sourceRepos);
     $.ajax({
@@ -14,10 +22,10 @@ $(function() {
             $("#github").append("Sorry, something went wrong.");
         }
     });
-});
 
-$(document).on("click", ".navbar-collapse.in", function(e) {
-    if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
-        $(this).collapse("hide");
-    }
+    $(document).on("click", ".navbar-collapse.in", function(e) {
+        if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
+            $(this).collapse("hide");
+        }
+    });
 });
