cd007d97d5057030880222a028d597d783e91053
Leo Hajder <leohajder@gmail.com>
2019-03-20 00:31:07 +0100

    Get data from json file in gist

--- src/js/main.js
+++ src/js/main.js
@@ -6,142 +6,35 @@ Handlebars.registerHelper('substr', function (length, context, options) {
       return context.substring(0, length) + "...";
     }
 
-    return context;
+    return context
   }
   
-  return null;
-});
+  return null
+})
 
 var template = Handlebars.templates['template']
 
-var data = {
-  age: function () {
-    var birthDay = '1987-11-14'
-    return new Date(new Date() - new Date(birthDay)).getFullYear() - 1970
-  },
-  job: {
-    title: 'junior developer',
-    company: 'MojPosao (Tau On-Line d.o.o.)',
-    site: 'https://www.moj-posao.net',
-    location: 'Zagreb, Croatia'
-  },
-  skills: [
-    {
-      skill: 'PHP',
-      subsets: [
-        'Symfony', 
-        'Sylius',
-      ]
-    }, 
-    {
-      skill: 'Python',
-      subsets: [
-        'Django', 
-        'Tkinter',
-      ]
-    }, 
-    {
-      skill: 'C#',
-      subsets: [
-        'Windows Forms', 
-        'ASP.Net MVC',
-      ]
-    }, 
-    {
-      skill: 'JavaScript',
-      subsets: [
-        'jQuery',
-      ]
-    }, 
-    {
-      skill: 'HTML & CSS',
-      subsets: [
-        'Bootstrap', 
-        'Twig',
-        'Jinja',
-        'Handlebars.js',
-        'Semantic UI',
-      ]
-    }, 
-    {
-      skill: 'databases & ORMs',
-      subsets: [
-        'MySQl',
-        'MS SQL Server',
-        'MongoDB',
-        'Doctrine',
-      ]
-    }, 
-    {
-      skill: 'Git'
-    }
-  ],
-  music: {
-    urls: [
-      'https://www.youtube.com/embed/videoseries?list=PLEC1A7D5D8F5E7203',
-      'https://www.youtube.com/embed/Xme5P55xZJw',
-      'https://www.youtube.com/embed/videoseries?list=PL392EC9FEC6CDE8DE',
-      'https://www.youtube.com/embed/videoseries?list=PL81F245C0FDAF7513',
-      'https://www.youtube.com/embed/videoseries?list=PL8D71B82E74CAB164',
-      'https://www.youtube.com/embed/Sl40IYO6KB0',
-      'https://www.youtube.com/embed/ikIlgcc2YSY',
-      'https://www.youtube.com/embed/SqwKEa3m8LU'
-    ],
-    frameBorder: '0'
-  },
-  contact: [
-    {
-      name: 'email',
-      url: 'mailto:leohajder@gmail.com',
-      icon: 'fa-envelope-o'
-    }, 
-    {
-      name: 'github',
-      url: 'https://github.com/leohajder',
-      icon: 'fa-github'
-    }, 
-    {
-      name: 'linkedin',
-      url: 'http://www.linkedin.com/in/leohajder',
-      icon: 'fa-linkedin'
-    }, 
-    {
-      name: 'freecodecamp',
-      url: 'https://www.freecodecamp.com/leohajder',
-      icon: 'fa-free-code-camp'
-    }, 
-    {
-      name: 'codepen',
-      url: 'https://codepen.io/leohajder/',
-      icon: 'fa-codepen'
-    }, 
-    {
-      name: 'facebook',
-      url: 'https://www.facebook.com/leohajder',
-      icon: 'fa-facebook'
-    }, 
-    {
-      name: 'youtube',
-      url: 'https://www.youtube.com/c/leohajder',
-      icon: 'fa-youtube-play'
-    }, 
-    {
-      name: 'lastfm',
-      url: 'https://www.last.fm/user/leohajder',
-      icon: 'fa-lastfm'
-    }
-  ],
-  repos: null
-}
+var contextData = {}
 
 $(function () {
-  $.ajax({url: 'https://api.github.com/users/leohajder/repos'}).done(function (response) {
-    data.repos = response
-  }).fail(function () {
-    data.repos = {}
-  }).always(function () {
-    $('#content').append(template(data))
+  $.ajax({
+    url: 'https://gist.githubusercontent.com/leohajder/3048708ff1d73968617c46792fe988f6/raw',
+    async: false
+  }).done(function (contextDataResponse) {
+    contextData = JSON.parse(contextDataResponse)
+    contextData.age = new Date(new Date() - new Date(contextData.birthDate)).getFullYear() - 1970
   })
+
+  $.ajax({
+    url: 'https://api.github.com/users/leohajder/repos',
+    async: false
+  }).done(function (reposResponse) {
+    contextData.repos = reposResponse
+  }).fail(function () {
+    contextData.repos = {}
+  })
+
+  $('#content').append(template(contextData))
 
   $(document).on('click', '.navbar-collapse.in', function (e) {
     if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
