d86a847ab4c0f2c58bcd03cb1cf7e4d489029bbb
Leo Hajder <leohajder@gmail.com>
2019-07-25 21:38:49 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -6,42 +6,42 @@ Handlebars.registerHelper('substr', function (length, context, options) {
       return context.substring(0, length) + '...';
     }
 
-    return context
+    return context;
   }
   
-  return null
-})
+  return null;
+});
 
-var template = Handlebars.templates['template']
+var template = Handlebars.templates['template'];
 
-var contextData = {}
+var contextData = {};
 
 $(function () {
   $.ajax({
     url: 'https://gist.githubusercontent.com/leohajder/3048708ff1d73968617c46792fe988f6/raw',
     async: false
   }).done(function (contextDataResponse) {
-    contextData = JSON.parse(contextDataResponse)
+    contextData = JSON.parse(contextDataResponse);
 
     if (contextData.birthDate) {
-      contextData.age = new Date(new Date() - new Date(contextData.birthDate)).getFullYear() - 1970
+      contextData.age = new Date(new Date() - new Date(contextData.birthDate)).getFullYear() - 1970;
     }
-  })
+  });
 
   $.ajax({
     url: 'https://api.github.com/users/leohajder/repos',
     async: false
   }).done(function (reposResponse) {
-    contextData.repos = reposResponse
+    contextData.repos = reposResponse;
   }).fail(function () {
-    contextData.repos = {}
-  })
+    contextData.repos = {};
+  });
 
-  $('#content').append(template(contextData))
+  $('#content').append(template(contextData));
 
   $(document).on('click', '.navbar-collapse.in', function (e) {
     if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
-      $(this).collapse('hide')
+      $(this).collapse('hide');
     }
   })
-})
+});
