56f6655c34c02983319a3e3185cb656f07ce64b6
Leo Hajder <leohajder@gmail.com>
2020-09-10 23:31:46 +0200

    Create initial layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
+    <AppHeader />
     <Nuxt />
+    <AppFooter />
   </div>
 </template>
+
+<script>
+import AppHeader from '../components/AppHeader';
+import AppFooter from '../components/AppFooter';
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
 
 <style>
 html {
