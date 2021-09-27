c211da5047b5208fae9b2a02382f36cf36760772
Leo Hajder <leohajder@gmail.com>
2021-09-27 03:29:25 +0200

    Switch to vanilla JS

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 function replaceUrlsWithLinks(str)
 {
-    const urlMatches = str.match(/\bhttps?\S+/gi);
-    const linkTemplate = `<a href="__url__" target="_blank">__url__</a>`;
+    var urlMatches = str.match(/\bhttps?\S+/gi);
+    var linkTemplate = '<a href="__url__" target="_blank">__url__</a>';
 
-    urlMatches.forEach((url) => {
-        const linkTag = linkTemplate.replaceAll('__url__', url);
+    urlMatches.forEach(function (url) {
+        var linkTag = linkTemplate.replaceAll('__url__', url);
         str = str.replaceAll(url, linkTag);
     });
 
     return str;
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('pre code').forEach((element) => {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('pre code').forEach(function (element) {
         hljs.highlightElement(element);
         element.innerHTML = replaceUrlsWithLinks(element.innerHTML);
     });
