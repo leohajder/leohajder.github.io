956b9ff682f6ea45b8c0cfcc1e9ad5a96347ccdb
Leo Hajder <leohajder@gmail.com>
2021-09-30 21:53:57 +0200

    Fix for IE

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@ function replaceUrlsWithLinks(str)
 {
     var urlMatches = str.match(/\bhttps?\S+/gi);
     var linkTemplate = '<a href="__url__" target="_blank">__url__</a>';
-
-    urlMatches.forEach(function (url) {
-        var linkTag = linkTemplate.replaceAll('__url__', url);
-        str = str.replaceAll(url, linkTag);
-    });
+    
+    for (var i = 0; i < urlMatches.length; i++) {
+        var linkTag = linkTemplate.replaceAll('__url__', urlMatches[i]);
+        str = str.replaceAll(urlMatches[i], linkTag);
+    }
 
     return str;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('pre code').forEach(function (element) {
-        hljs.highlightElement(element);
-        element.innerHTML = replaceUrlsWithLinks(element.innerHTML);
-    });
+    var codeSnippets = document.querySelectorAll('pre code');
+    for (var i = 0; i < codeSnippets.length; i++) {
+        hljs.highlightElement(codeSnippets[i]);
+        codeSnippets[i].innerHTML = replaceUrlsWithLinks(codeSnippets[i].innerHTML);
+    }
 });
