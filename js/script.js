814bd44eb14114d20bfc8722e4fac3f3d822bbc8
Leo Hajder <leohajder@gmail.com>
2023-03-02 02:09:21 +0100

    Do not replace links

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
-function replaceUrlsWithLinks(str)
-{
-    var urlMatches = str.match(/\bhttps?\S+/gi);
-    var linkTemplate = '<a href="__url__" target="_blank">__url__</a>';
-    
-    for (var i = 0; i < urlMatches.length; i++) {
-        var linkTag = linkTemplate.replaceAll('__url__', urlMatches[i]);
-        str = str.replaceAll(urlMatches[i], linkTag);
-    }
-
-    return str;
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     var codeSnippets = document.querySelectorAll('pre code');
     for (var i = 0; i < codeSnippets.length; i++) {
         hljs.highlightElement(codeSnippets[i]);
-        codeSnippets[i].innerHTML = replaceUrlsWithLinks(codeSnippets[i].innerHTML);
     }
 });
